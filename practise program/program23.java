ee7348b432319ecb4925e49eef2c936d5f9e0c28
S Muhamed Amreeen Sadica <89631832+amreensadica18@users.noreply.github.com>
2023-03-22 21:37:32 +0530

    Create program23.java

---
+++ practise program/program23.java
@@ -0,0 +1,11 @@
+public class TestArray {
+public static void main(String[] args) {
+      double[] myList = {1.9, 2.9, 3.4, 3.5};
+     // Finding the largest element
+      double max = myList[0];
+      for (int i = 1; i < myList.length; i++) {
+         if (myList[i] > max) max = myList[i];
+      }
+      System.out.println("Max is " + max);  
+   }
+}
