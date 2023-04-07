27781ec71f3195b0f3ad706236b40712325fa40e
S Muhamed Amreeen Sadica <89631832+amreensadica18@users.noreply.github.com>
2023-04-07 06:33:06 +0530

    Create program35.java

---
+++ practise program/program35.java
@@ -0,0 +1,7 @@
+public class StringExample{  
+public static void main(String args[]){  
+String s1="java";  
+String s2="programming";  
+System.out.println("string length is: "+s1.length());
+System.out.println("string length is: "+s2.length()); 
+}}
