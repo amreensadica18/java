f9fbf015b01fe189873bfe4197c38c02ad90dfb1
S Muhamed Amreeen Sadica <89631832+amreensadica18@users.noreply.github.com>
2023-04-26 22:45:17 +0530

    Create program47.java

---
+++ practise program/program47.java
@@ -0,0 +1,14 @@
+package com.sample;
+public class Test {
+final public static void sample(String a) {
+System.out.println(a);
+}
+final public static void sample(String a, int no) {
+System.out.println(a);
+System.out.println(no);
+}
+public static void main(String[] args) {
+sample("Prabha");
+sample("Haran", 5);
+}
+}
