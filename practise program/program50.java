d03389086337132dfda0aa700f4a5d160e8fdb04
S Muhamed Amreeen Sadica <89631832+amreensadica18@users.noreply.github.com>
2023-04-29 22:05:52 +0530

    Create program50.java

---
+++ practise program/program50.java
@@ -0,0 +1,10 @@
+public class LoopExample1 {
+    public static void main(String[] args) {
+        for(int i=0;i<10;i++){
+             System.out.println("Prabha");
+             for(int j=i;j<=i;j++){
+              System.out.println("Haran");
+        }
+        }
+    }
+}
