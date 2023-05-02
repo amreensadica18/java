f52b8d00e7d9ca0b754921e437ae97c42af56437
S Muhamed Amreeen Sadica <89631832+amreensadica18@users.noreply.github.com>
2023-05-02 22:45:35 +0530

    Create program51.java

---
+++ practise program/program51.java
@@ -0,0 +1,13 @@
+package com.aiite.sam2;
+public class Test3 {
+public static void main(String[] args) {
+int age = 17;
+if (age>=18 && age<=59) {
+System.out.println("Major");
+} else if (age>=60) {
+System.out.println("Senior");
+} else if (age<18) {
+System.out.println("Minor");
+}
+}
+}
