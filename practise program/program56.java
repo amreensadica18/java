73d5c566413ead4f2a43a2e3524b2ef076f7a8f2
S Muhamed Amreeen Sadica <89631832+amreensadica18@users.noreply.github.com>
2023-05-08 20:52:24 +0530

    Create program56.java

---
+++ practise program/program56.java
@@ -0,0 +1,11 @@
+package com.sample;
+public class NestedForloop {
+public static void main(String[] args) {
+for (int i = 0; i < 2; i++) {
+System.out.println("Outer World");
+}
+for (int i = 0; i < 3; i++) {
+System.out.println("Seperate World");
+}
+}
+}
