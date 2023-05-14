49a066b8750e63b5a6c2eca40996ae044ffb171a
S Muhamed Amreeen Sadica <89631832+amreensadica18@users.noreply.github.com>
2023-05-14 22:02:13 +0530

    Create Program60.java

---
+++ practise program/Program60.java
@@ -0,0 +1,17 @@
+package com.sample;
+
+public class MultidimentionalArray {
+
+public static void main(String[] args) {
+
+int[][] f = { {5,10,15,20},{25,30,35,40},{45,50,55,60} };
+
+System.out.println(f[0][2]);
+
+System.out.println(f[1][2]);
+
+System.out.println(f[2][3]);
+
+}
+
+}
