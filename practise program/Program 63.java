e507d787e7197a77cf2133a6fbf99b1e1cced06e
S Muhamed Amreeen Sadica <89631832+amreensadica18@users.noreply.github.com>
2023-05-17 21:52:01 +0530

    Create Program 63.java

---
+++ practise program/Program 63.java
@@ -0,0 +1,10 @@
+package com.sample;
+import java.util.Arrays;
+public class Arrays {
+public static void main(String[] args) {
+int[] a = {10,15,20,25,45,55,85,1,2,9,53};
+Arrays.sort(a);
+String string = Arrays.toString(a);
+System.out.println(string);
+}
+}
