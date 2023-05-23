a22ede643fcf29e03433f4d2929f7536306664e4
S Muhamed Amreeen Sadica <89631832+amreensadica18@users.noreply.github.com>
2023-05-23 22:38:01 +0530

    Create Program68.java

---
+++ practise program/Program68.java
@@ -0,0 +1,13 @@
+public class CharString {
+
+  public static void main(String[] args) {
+
+        char ch = 'a';
+
+        String st = Character.toString(ch);
+
+        System.out.println("The string is: " + st);
+
+    }
+
+}
