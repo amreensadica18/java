04bf365519e6812baa6a92dbf3a254e1a8352120
S Muhamed Amreeen Sadica <89631832+amreensadica18@users.noreply.github.com>
2023-05-25 20:49:44 +0530

    Create Program69.java

---
+++ practise program/Program69.java
@@ -0,0 +1,19 @@
+public class Main {
+
+  public static void main(String[] args) {
+
+    long a = 23223771L;
+
+    long b = 52341861L;
+
+    int c = (int)a;
+
+    int d = (int)b;
+
+    System.out.println(c);   
+
+    System.out.println(d);  
+
+  }
+
+}
