d6af405144f041e77426408df17462cae78a2901
S Muhamed Amreeen Sadica <89631832+amreensadica18@users.noreply.github.com>
2023-05-27 22:06:46 +0530

    Create Program70.java

---
+++ practise program/Program70.java
@@ -0,0 +1,19 @@
+public class IntToChar {
+
+  public static void main(String[] args) {
+
+    int num1 = 89;
+
+    int num2 = 44;
+
+    char a = (char)num1;
+
+    char b = (char)num2;
+
+    System.out.println(a);    
+
+    System.out.println(b);   
+
+  }
+
+}
