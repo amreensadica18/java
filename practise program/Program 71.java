db3a8f96acd44f32c39c6c3c0e991ad2064c6629
S Muhamed Amreeen Sadica <89631832+amreensadica18@users.noreply.github.com>
2023-05-28 21:03:12 +0530

    Create Program 71.java

---
+++ practise program/Program 71.java
@@ -0,0 +1,19 @@
+public class BooleanToString {
+
+  public static void main(String[] args) {
+
+    boolean booleanValue1 = false;
+
+    boolean booleanValue2 = true;
+
+    String stringValue1 = String.valueOf(booleanValue1);
+
+    String stringValue2 = String.valueOf(booleanValue2);
+
+    System.out.println(stringValue1);    
+
+    System.out.println(stringValue2);    
+
+  }
+
+}
