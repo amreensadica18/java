ea9a90af7724cef96ae02293d4186f6eda27e03d
S Muhamed Amreeen Sadica <89631832+amreensadica18@users.noreply.github.com>
2023-05-31 22:47:41 +0530

    Create Program73.java

---
+++ practise program/Program73.java
@@ -0,0 +1,13 @@
+public class IntToString {
+  public static void main(String[] args) {
+
+    int num1 = 56;
+    int num2 = 88;
+
+    String str1 = String.valueOf(num1);
+    String str2 = String.valueOf(num2);
+
+    System.out.println(str1);    
+    System.out.println(str2);   
+  }
+}
