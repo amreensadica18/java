2c6bbcf359cf8af0c000193570759ee046284785
S Muhamed Amreeen Sadica <89631832+amreensadica18@users.noreply.github.com>
2023-06-02 21:29:34 +0530

    Create Program74.java

---
+++ practise program/Program74.java
@@ -0,0 +1,19 @@
+public class IntToDouble {
+
+  public static void main(String[] args) {
+
+    int a =93;
+
+    int b = 79;
+
+    double c = a;
+
+    double d = b;
+
+    System.out.println(c);  
+
+    System.out.println(d);
+
+  }
+
+}
