ba919585fa0a4b87a77aa05ad6e6fb2d8b194ef9
S Muhamed Amreeen Sadica <89631832+amreensadica18@users.noreply.github.com>
2023-06-16 22:35:27 +0530

    Create Program 86.java

---
+++ practise program/Program 86.java
@@ -0,0 +1,28 @@
+public class Prime {
+
+    public static void main(String[] args) {
+
+        int low = 10, high = 40;
+
+        while (low < high) {
+            if(checkPrimeNumber(low))
+                System.out.print(low + " ");
+
+            ++low;
+        }
+    }
+
+    public static boolean checkPrimeNumber(int num) {
+        boolean flag = true;
+
+        for(int i = 2; i <= num/2; ++i) {
+
+            if(num % i == 0) {
+                flag = false;
+                break;
+            }
+        }
+
+        return flag;
+    }
+}
